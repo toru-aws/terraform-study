f3012f901a9f99ead79184d93e51ba21610d5f38
toru <29oishi4@gmail.com>
2025-10-11 21:46:36 +0900

    Trigger Terraform + Ansible CI/CD

---
+++ modules/s3/main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "springboot_app" {
+  bucket = "my-springboot-app-2025"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "SpringBootApp"
+    Environment = "Dev"
+  }
+}
+
+output "springboot_bucket_name" {
+  value = aws_s3_bucket.springboot_app.bucket
+}
